8cf543924d83ae58233077bd413dc8735e5f47b5
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-11-07 22:42:43 +0900

    first commit

---
+++ go-to_do/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", echoHello)
+	http.ListenAndServe(":8000", nil)
+}
+
+func echoHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
