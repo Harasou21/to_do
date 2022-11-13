b00e141fc00566c9b2e95671b9073156e3ae5e77
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-11-13 14:49:46 +0900

    コンポーネントを読み込んだ

--- react-ts-to_do/src/index.tsx
+++ react-ts-to_do/src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './components/Hello'
+import Message from './components/Message';
+import Name from './components/Name'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello />
+    <Message />
+    <Name />
   </React.StrictMode>
 );
 
