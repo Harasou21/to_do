5d4d574e85c113be609097c04546652f43e52725
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-11-29 22:26:10 +0900

    チャネルについて

--- go-to_do/main.go
+++ go-to_do/main.go
@@ -1,24 +1,21 @@
 package main
-
-import (
-	"fmt"
-)
-
-func goroutine(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum
+ 
+import "fmt"
+ 
+func goroutine(s []string, c chan string){
+    sum := ""
+    for _, v := range s{
+        sum += v
+        c <- sum
+    }
+		close(c)
 }
-
-func main() {
-	s := []int{1, 2, 3, 4, 5}
-	c := make(chan int)
-	go goroutine(s, c)
-	go goroutine(s, c)
-	x := <-c
-	fmt.Println(x)
-	y := <-c
-	fmt.Println(y)
+ 
+func main(){
+    words := []string{"test1!", "test2!", "test3!", "test4!"}
+    c := make(chan string)
+    go goroutine(words, c)
+    for w := range c{
+        fmt.Println(w)
+    }
 }
